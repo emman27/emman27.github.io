36094d2007fa7f3cc03f382276300e3f0ad74b72
emman <eygohlolz@gmail.com>
2015-01-20 11:14:00 +0800

    Button fix

--- js/activate_buttons.js
+++ js/activate_buttons.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     var ministryPlansVisible = false;
-    $("#ministryPlansBtn").onclick(function(){
+    $("#ministryPlansBtn").click(function(){
+        console.log("clicked");
         if(!ministryPlansVisible){
-            $("#ministryPlans").html('<h3>Ministry Plans</h3><button class="btn btn-default" id="ministryPlansBtn">Hide</button><iframe src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0Am-af6IxPEyydDJncXRaWDBQclVmbW1iMzVnUjdVd1E&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650" width='100%' height='650' frameborder='0'></iframe>');
+            $("#ministryPlans").append('<iframe id="ministryPlanTimeline" src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0Am-af6IxPEyydDJncXRaWDBQclVmbW1iMzVnUjdVd1E&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650" width="100%" height="650" frameborder="0"></iframe>');
             ministryPlansVisible = true;
         }else{
-            $("ministryPlans").html('<h3>Ministry Plans</h3><button class="btn btn-default" id="ministryPlansBtn">Show</button>');
+            $("#ministryPlanTimeline").remove();
+            ministryPlansVisible = false;
         }
     })
 })
--- js/remove_bible_gateway.js
+++ js/remove_bible_gateway.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
     var stuff = $(".jumbotron").html();
-    console.log(stuff.substring(0, stuff.indexOf("</a>")) + "</a></div></h3>");
     var newStuff = stuff.substring(0, stuff.indexOf("</a>")) + "</a></div></h3>"
     $(".jumbotron").html(newStuff);
-    console.log($(".jumbotron"));
-    console.log($(".jumbotron").html());
 })
