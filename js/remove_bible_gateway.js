4a1509a1b3832adbd2144c3db1f61b9d3aa12f33
Emman <emmanuel.goh.7@gmail.com>
2016-10-18 15:23:54 +0800

    Remove old files

--- js/remove_bible_gateway.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    var stuff = $(".jumbotron").html();
-    var newStuff = stuff.substring(0, stuff.indexOf("</a>")) + "</a></div></h3>"
-    $(".jumbotron").html(newStuff);
-})
