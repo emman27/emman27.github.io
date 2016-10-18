2f26105f64043d6693948f92a051464c3c96fe12
Emman <emmanuel.goh.7@gmail.com>
2016-10-18 22:35:27 +0800

    Mega update with scrollspy

---
+++ js/index.js
@@ -0,0 +1,19 @@
+(function(){
+  var getControlledElement = function(element){
+    return $($(element).data('toggle'));
+  }
+
+  $('.control').on('scrollSpy:enter', function(){
+    var open = getControlledElement(this);
+    open.show('slide');
+  });
+
+  // Never hide stuff. This gives smooth scrolling
+  // $('.control').on('scrollSpy:exit', function(){
+  //   var open = getControlledElement(this);
+  //   open.hide('slide');
+  // });
+
+  $('.control').scrollSpy();
+
+})();
